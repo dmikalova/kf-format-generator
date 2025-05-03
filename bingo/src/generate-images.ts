ad1146000deb2e46760feb365f508c9b50db7abd
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-05-02 21:05:47 -0700

    feat: add bingo title

--- bingo/src/generate-images.ts
+++ bingo/src/generate-images.ts
@@ -16,6 +16,7 @@ const margin = 25
 const border = 5
 const textSize = squareSize - margin * 2 - border * 2
 const backgroundSize = textSize - 25
+const cardSize = squareSize * 6
 
 if (gen) {
   spawn.sync('rm', ['-rf', './squares', './assets-resized', './cards'], io)
@@ -28,14 +29,13 @@ if (gen) {
   args.push(...['-size', `${textSize}x${textSize}`])
   args.push(...['canvas:transparent'])
   args.push(...[`PNG32:./assets-resized/canvas.png`])
-
   spawn.sync('magick', args, io)
 
   // resize assets
-  fs.readdirSync(path.join(__dirname, '../', 'assets'))
+  fs.readdirSync(path.join(__dirname, '../', 'assets', 'square-backgrounds'))
     .filter((file) => file.endsWith('.png'))
     .forEach((file) => {
-      const filepath = path.join(__dirname, '../', 'assets', file)
+      const filepath = path.join(__dirname, '../', 'assets', 'square-backgrounds', file)
 
       // resize smaller than canvas
       let args = []
@@ -200,13 +200,119 @@ function generateBoard(index: number) {
   args.push(...[`./cards/card${index.toString().padStart(4, '0')}.png`])
   spawn.sync('magick', args, io)
 
+  // args = []
+  // args.push(...[`./cards/card${index.toString().padStart(4, '0')}.png`])
+  // args.push(...['-bordercolor', 'gold1', '-border', border])
+  // args.push(...[`./cards/card${index.toString().padStart(4, '0')}.png`])
+  // spawn.sync('magick', args, io)
+}
+
+if (gen) {
+  for (let i = 0; i < boardCount; i++) {
+    generateBoard(i)
+  }
+}
+
+// resize logo and make canvas
+let args = []
+args.push(...['./assets/kf-logo-flat.png'])
+args.push(...['-resize', `x${backgroundSize}`])
+args.push(...[`./assets-resized/kf-logo-flat.png`])
+spawn.sync('magick', args, io)
+
+args = []
+args.push(...['./assets/kf-logo.png'])
+args.push(...['-resize', `x${backgroundSize}`])
+args.push(...[`./assets-resized/kf-logo.png`])
+spawn.sync('magick', args, io)
+
+args = []
+args.push(...['-size', `${cardSize - border * 2}x${squareSize - border * 2}`])
+args.push(...['canvas:transparent'])
+args.push(...[`PNG32:./assets-resized/canvas-title.png`])
+spawn.sync('magick', args, io)
+
+function generateTitle(index: number) {
+  // prefix
+  const prefixes = data.title.prefixes
+  const prefix = prefixes[Math.floor(Math.random() * prefixes.length)]
+
+  const xFactor = 3
+  const yFactor = 3
+
+  let args = []
+  args.push(...['-background', 'none'])
+  args.push(...['-gravity', 'center'])
+  args.push(...['-font', 'TeutonFett'])
+  args.push(...['-size', `${cardSize / xFactor}x${squareSize / yFactor}`])
+  args.push(...[`caption:The ${prefix}`])
+  args.push(...[`./cards/prefix${index.toString().padStart(4, '0')}.png`])
+
+  spawn.sync('magick', args, io)
+
+  // suffix
+  const suffixes = data.title.suffixes
+  const suffix = suffixes[Math.floor(Math.random() * suffixes.length)]
+
   args = []
-  args.push(...[`./cards/card${index.toString().padStart(4, '0')}.png`])
+  args.push(...['-background', 'none'])
+  args.push(...['-gravity', 'center'])
+  args.push(...['-font', 'TeutonFett'])
+  args.push(...['-size', `${cardSize / xFactor}x${squareSize / yFactor}`])
+  args.push(...[`label:${suffix}`])
+  args.push(...[`./cards/suffix${index.toString().padStart(4, '0')}.png`])
+  spawn.sync('magick', args, io)
+
+  // Randomly pick from an array of 2 items
+  const logos = [`./assets-resized/kf-logo.png`, `./assets-resized/kf-logo-flat.png`]
+  const randomLogo = logos[Math.floor(Math.random() * logos.length)]
+
+  args = []
+  args.push(...['-gravity', 'center'])
+  args.push(...['-colorspace', 'sRGB'])
+  args.push(...[`./assets-resized/canvas-title.png`])
+  args.push(...[randomLogo, `-geometry`, `+0+0`, `-composite`])
+  args.push(...[`./cards/prefix${index.toString().padStart(4, '0')}.png`, `-geometry`, `-800-150`, `-composite`])
+  args.push(...[`./cards/suffix${index.toString().padStart(4, '0')}.png`, `-geometry`, `+500+150`, `-composite`])
+  args.push(...[`./cards/title${index.toString().padStart(4, '0')}.png`])
+  spawn.sync('magick', args, io)
+
+  // add border
+  args = []
+  args.push(...[`./cards/title${index.toString().padStart(4, '0')}.png`])
+  args.push(...['-bordercolor', 'white', '-border', 0])
   args.push(...['-bordercolor', 'gold1', '-border', border])
-  args.push(...[`./cards/card${index.toString().padStart(4, '0')}.png`])
+  args.push(...[`./cards/title${index.toString().padStart(4, '0')}.png`])
   spawn.sync('magick', args, io)
 }
 
+// if (gen) {
 for (let i = 0; i < boardCount; i++) {
-  generateBoard(i)
+  generateTitle(i)
 }
+// }
+
+// put it all together
+
+function assembleCard(index: number) {
+  let args = []
+  args.push(...[`./cards/title${index.toString().padStart(4, '0')}.png`])
+  args.push(...[`./cards/card${index.toString().padStart(4, '0')}.png`])
+  args.push(...['-append'])
+  args.push(...[`./cards/board${index.toString().padStart(4, '0')}.png`])
+  spawn.sync('magick', args, io)
+
+  // add border
+  args = []
+  args.push(...[`./cards/board${index.toString().padStart(4, '0')}.png`])
+  args.push(...['-bordercolor', 'white', '-border', 0])
+  args.push(...['-bordercolor', 'gold1', '-border', border])
+  args.push(...[`./cards/board${index.toString().padStart(4, '0')}.png`])
+  spawn.sync('magick', args, io)
+}
+
+// if (gen) {
+for (let i = 0; i < boardCount; i++) {
+  assembleCard(i)
+}
+// }
